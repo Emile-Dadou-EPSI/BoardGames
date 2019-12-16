dc5b625b988921d70a70fad3b23d93dcfc7af47b
emile <emile.dadou@epsi.fr>
2019-12-16 12:42:46 +0100

    Add data read and Login servlet

--- src/main/java/BoardGames/servlets/ServletLogin.java
+++ src/main/java/BoardGames/servlets/ServletLogin.java
@@ -1,24 +1,28 @@
+package BoardGames.servlets;
+
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import jdk.internal.jimage.ImageStream;
 
+import javax.servlet.RequestDispatcher;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-
-public class testfirebase {
-    // Use the application default credentials
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+@javax.servlet.annotation.WebServlet(name = "ServletLogin")
+public class ServletLogin extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        // Récupération des infos du formulaire de connexion
+        RequestDispatcher rd = null;
+        String Login = request.getParameter("login");
+        String Pwd = request.getParameter("password");
+        System.out.println(Login);
+        // Connexion to cloud firestore
         FileInputStream refreshToken = new FileInputStream("C:\\Users\\emile\\IdeaProjects\\BoardGames\\src\\main\\java\\boardgames-6813a-firebase-adminsdk-vvivt-657fd5ca31.json");
 
         GoogleCredentials credentials = GoogleCredentials.fromStream(refreshToken);
@@ -27,20 +31,8 @@ public class testfirebase {
 
         Firestore db = FirestoreClient.getFirestore();
 
-        // Add document data with id "test"
-        Map<String, Object> data = new HashMap<>();
-        data.put("Name", "Emile");
-        data.put("LastName", "Dadou");
-        data.put("Name", "Lionel");
-        data.put("LastName", "Batt");
-        data.put("Password", "test");
-        //db.collection("FirstTest").document("Test1").set(data);
-        ApiFuture<WriteResult> res = db.collection("users").document("user1").set(data);
-        System.out.print("Update time : " + res.get().getUpdateTime());
-
-
-        DocumentReference docRef = db.collection("users").document("user1");
-        ApiFuture<DocumentSnapshot> future = docRef.get();
+        DocumentReference docRef2 = db.collection("users").document("user1");
+        ApiFuture<DocumentSnapshot> future = docRef2.get();
 
         DocumentSnapshot document = null;
         try {
@@ -52,10 +44,19 @@ public class testfirebase {
         }
         if (document.exists()) {
             System.out.println("Document data: " + document.getData());
-            System.out.println("Hello");
         } else {
             System.out.println("No such document!");
         }
+
+        if (Login.equals("test") && Pwd.equals("test")) {
+            request.getRequestDispatcher("/index.jsp").forward(request,response);
+        }
+        else {
+            request.getRequestDispatcher("/test.html").forward(request,response);
+        }
     }
 
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -11,6 +11,6 @@
     <title>$Title$</title>
   </head>
   <body>
-  $END$
+  HELLO Connexion worked out fine.
   </body>
 </html>
